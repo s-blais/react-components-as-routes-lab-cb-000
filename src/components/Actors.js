f8641ece981dcc1db4a03080ffad33a1ee018e49
s-blais <stephen@s-blais.com>
2021-02-11 14:01:27 -0500

    commit tree 04d685329c7b773b5d22796ee3dbbb3e1a1474d1

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { actors } from '../data';
 
+const actorEntry = (actor, idx) => {
+  return (
+    <div key={ idx }>
+      Name: { actor.name }
+      <ul>
+        { actor.movies.map((movie, idx) => <li key={idx}>{movie}</li>)}
+      </ul>
+    </div>
+  )
+}
+
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      { actors.map((actor, idx) => actorEntry(actor, idx)) }
     </div>
   );
 };
